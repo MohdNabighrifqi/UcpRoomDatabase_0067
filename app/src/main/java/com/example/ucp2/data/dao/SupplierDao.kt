bcbaa5a7c207d720bef42e4523704c7851690b09
MohdNabighrifqi <nabigh.boedax@gmail.com>
2024-12-24 13:03:54 +0800

    Membuat File Dao Supplier

---
+++ app/src/main/java/com/example/ucp2/data/dao/SupplierDao.kt
@@ -0,0 +1,19 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SupplierDao {
+    @Insert
+    suspend fun insertSupplier(supplier: Supplier)
+
+    @Query("SELECT * FROM supplier  ORDER BY nama ASC")
+    fun getAllSupplier(): Flow<List<Supplier>>
+
+    @Query("SELECT * FROM supplier WHERE id = :id")
+    fun getSupplier(id: String): Flow<Supplier>
+}
